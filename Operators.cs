d541117772ba00b5d652063f41238797f4c89d09
Devesh Singh <deveshsingh.8865@gmail.com>
2026-08-05 08:45:36 +0530

    Add files via upload

---
+++ Operators.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The Addition of two Numbers is: " + (a + b));
+            Console.WriteLine("The Product of two numbers is: " + (a * b));
+            Console.WriteLine("The difference of two numbers is : " + (a - b));
+            Console.WriteLine("The quotient of two numbers is: " + (a / b));
+            Console.WriteLine("The remiander of two numbers is : " + (a % b));
+        }
+    }
+}
