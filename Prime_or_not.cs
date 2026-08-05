e5bf76c922c494069bfc5fee8e6ed6cfbf40dd7c
Devesh Singh <deveshsingh.8865@gmail.com>
2026-08-05 08:36:42 +0530

    Add files via upload

---
+++ Prime_or_not.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Prime_or_not
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 29;
+            if (n <= 1)
+            {
+                Console.WriteLine("Prime number is not possible ");
+            }
+            if(n == 2)
+            {
+                Console.WriteLine("Prime number");
+            }
+            if (n % 2 == 0)
+            {
+                Console.WriteLine("Number is not prime");
+            }
+            for(int i = 3;i * i<= n;i+= 2)
+            {
+                if(n % 3 == 0)
+                {
+                    Console.WriteLine("Number is not Prime ");
+                }
+                else
+                {
+                    Console.WriteLine("Number is Prime");
+                }
+            }
+
+        }
+    }
+}
