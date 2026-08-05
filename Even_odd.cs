14d0de9ce7708e89788fae7b7dde5626ca56f4c6
Devesh Singh <deveshsingh.8865@gmail.com>
2026-08-05 08:40:15 +0530

    Add files via upload

---
+++ Even_odd.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Even_odd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            Console.WriteLine("Enter the random number : ");
+            N = Convert.ToInt32(Console.ReadLine());
+
+            for( int i =1;i<= N; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine("The given number is Even: " + i);
+                }
+                else
+                {
+                    Console.WriteLine("The given number is Odd: "+ i);
+                }
+            }
+        }
+    }
+}
