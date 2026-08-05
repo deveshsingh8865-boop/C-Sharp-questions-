580bc67e7c5a843e12f88849c03d97b241f3522e
Devesh Singh <deveshsingh.8865@gmail.com>
2026-08-05 08:41:20 +0530

    Add files via upload

---
+++ Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            Console.WriteLine("Enter the number a:");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the number b:");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the number c:");
+            int c = Convert.ToInt32(Console.ReadLine());
+            switch (c)
+            {
+                case 1: Console.WriteLine("The addition is "+ (a+b)); 
+                    break;
+                case 2: Console.WriteLine("The difference is" + (a - b));
+                    break;
+                case 3: Console.WriteLine("The product is : " + (a * b));
+                    break;
+                case 4: Console.WriteLine("The division is : " + (a / b));
+                    break;
+
+            }
+        }
+    }
+}
