4f723752b54d28d475a8888ad0b32e376c7c2a5b
Devesh Singh <deveshsingh.8865@gmail.com>
2026-08-05 08:44:25 +0530

    Add files via upload

---
+++ Number_nature.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number_nature
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine("Enter the number : ");
+            a = Convert.ToInt32(Console.ReadLine());
+            if(a > 0)
+            {
+                Console.WriteLine("The number "+a+" is positive");
+            }
+            else if(a < 0)
+            {
+                Console.WriteLine("The number " + a + " is negative");
+            }
+            else
+            {
+                Console.WriteLine("The number " + a + " is Zero");
+            }
+        }
+    }
+}
