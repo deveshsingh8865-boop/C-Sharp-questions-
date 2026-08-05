544029b7b1106e55e09444c9a8b05aaffe6db670
Devesh Singh <deveshsingh.8865@gmail.com>
2026-08-05 08:48:23 +0530

    Add files via upload

---
+++ Sum_N_numbers.cs
@@ -0,0 +1,18 @@
+﻿//Write a program of compute the sum of first N numbers (without using loop)
+using System;
+using System.Runtime.ExceptionServices;
+namespace DeveshConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int num;
+            Console.WriteLine("Ente the Number: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            sum = (num * (num + 1) / 2);
+            Console.WriteLine("The Sum of N numbers :"+sum);
+        }
+    }
+}
