c51336ed4904f64948d8fd71eb0e893903450d7b
Devesh Singh <deveshsingh.8865@gmail.com>
2026-08-05 08:39:07 +0530

    Add files via upload

---
+++ Factorial.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+            int f = 1;
+            for (int i = 0;i < n; i++)
+            {
+                f = f * (n - i);
+                //Console.WriteLine("The factorial of the given number is : " + f);
+            }
+            Console.WriteLine("The factorial of the given number is : " + f);
+
+        }
+    }
+}
